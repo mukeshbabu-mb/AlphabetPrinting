d63eb006e3c650d032cdefb6da66fbe6ea66f81e
mukeshbabu-mb <142506661+mukeshbabu-mb@users.noreply.github.com>
2023-10-18 08:43:31 +0530

    Add files via upload

--- AlphabetLetters.java
+++ AlphabetLetters.java
@@ -68,18 +68,151 @@ public class AlphabetLetters {
 	}
 
 	//=============================================================================================
-		public void letterF() {
-			for(int row=1;row<=10;row++) {
-				for(int col=1;col<=10;col++) {
-					if(row==1||col==1||row==5) {
-						System.out.print("*");
-					}else {
-						System.out.print(" ");
-					}
+	public void letterF() {
+		for(int row=1;row<=10;row++) {
+			for(int col=1;col<=10;col++) {
+				if(row==1||col==1||row==5) {
+					System.out.print("*");
+				}else {
+					System.out.print(" ");
 				}
-				System.out.println();
 			}
+			System.out.println();
 		}
+	}
+	//=============================================================================================
+	public void letterG() {
+		for(int row=1;row<=10;row++) {
+			for(int col=1;col<=10;col++) {
+				if(row==1||col==1||(row==5 && col>=5)||(col==5&&row>=5)||(col==10&&row>=5)||(row==10&&col<=5)) {
+					System.out.print("*");
+				}else {
+					System.out.print(" ");
+				}
+			}
+			System.out.println();
+		}
+	}
+	//=============================================================================================
+	public void letterH() {
+		for(int row=1;row<=10;row++) {
+			for(int col=1;col<=10;col++) {
+				if(row==5||col==1||col==10) {
+					System.out.print("*");
+				}else {
+					System.out.print(" ");
+				}
+			}
+			System.out.println();
+		}
+	}
+	//=============================================================================================
+	public void letterI() {
+		for(int row=1;row<=10;row++) {
+			for(int col=1;col<=10;col++) {
+				if(col==5||row==1||row==10) {
+					System.out.print("*");
+				}else {
+					System.out.print(" ");
+				}
+			}
+			System.out.println();
+		}
+	}
+	//=============================================================================================
+	public void letterJ() {
+		for(int row=1;row<=10;row++) {
+			for(int col=1;col<=10;col++) {
+				if(row==1||col==6||(row==10&&col<=6)||(col==1&&row>5)) {
+					System.out.print("*");
+				}else {
+					System.out.print(" ");
+				}
+			}
+			System.out.println();
+		}
+	}
+	//=============================================================================================
+	public void letterK() {
+		for(int row=1;row<=10;row++) {
+			for(int col=1;col<=10;col++) {
+				if(col==1||(row+col==7)||(row-col==4)) {
+					System.out.print("*");
+				}else {
+					System.out.print(" ");
+				}
+			}
+			System.out.println();
+		}
+	}
+	//=============================================================================================
+	public void letterL() {
+		for(int row=1;row<=10;row++) {
+			for(int col=1;col<=10;col++) {
+				if(col==1||row==10) {
+					System.out.print("*");
+				}else {
+					System.out.print(" ");
+				}
+			}
+			System.out.println();
+		}
+	}
+	//=============================================================================================
+	public void letterM() {
+		for(int row=1;row<=10;row++) {
+			for(int col=1;col<=10;col++) {
+				if(col==1||col==10||(row==col&&row<=5)||(row+col==11&&col>5)) {
+					System.out.print("*");
+				}else {
+					System.out.print(" ");
+				}
+			}
+			System.out.println();
+		}
+	}
+	//=============================================================================================
+	public void letterN() {
+		for(int row=1;row<=10;row++) {
+			for(int col=1;col<=10;col++) {
+				if(col==1||col==10||(row==col)) {
+					System.out.print("*");
+				}else {
+					System.out.print(" ");
+				}
+			}
+			System.out.println();
+		}
+	}
+
+	//=============================================================================================
+	public void letterS() {
+		for(int row=1;row<=10;row++) {
+			for(int col=1;col<=10;col++) {
+				if(row==1||(col==1&&row<=5)||row==5||(col==10&&row>=5)||row==10) {
+					System.out.print("*");
+				}else {
+					System.out.print(" ");
+				}
+			}
+			System.out.println();
+		}
+	}
+	//=============================================================================================
+	public void letterT() {
+		for(int row=1;row<=10;row++) {
+			for(int col=1;col<=10;col++) {
+				if(row==1||col==5) {
+					System.out.print("*");
+				}else {
+					System.out.print(" ");
+				}
+			}
+			System.out.println();
+		}
+	}
+
+
 
 
 }
--- LetterFinder.java
+++ LetterFinder.java
@@ -16,6 +16,28 @@ public class LetterFinder {
 		break;
 		case 'F': letters.letterF();
 		break;
+		case 'G': letters.letterG();
+		break;
+		case 'H': letters.letterH();
+		break;
+		case 'I': letters.letterI();
+		break;
+		case 'J': letters.letterJ();
+		break;
+		case 'K': letters.letterK();
+		break;
+		case 'L': letters.letterL();
+		break;
+		case 'M': letters.letterM();
+		break;
+		case 'N': letters.letterN();
+		break;
+		case 'S': letters.letterS();
+		break;
+		case 'T': letters.letterT();
+		break;
+		
+		
 		default:printingStarLine();
 		System.out.println("Entered Letter is not Created by the developer.");
 		System.out.println("The Letter '"+vul+"' will be added soon :) ");
